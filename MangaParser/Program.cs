d12f51138a14d413bd2564383acf05bb4d08f4b4
TheSetaSimo <njetmolotoffnet@mail.ru>
2023-03-08 01:23:19 +0300

    deleted urls

--- MangaParser/Program.cs
+++ MangaParser/Program.cs
@@ -1,7 +1,7 @@
 ﻿using MangaParser.Application.Services;
 using MangaParser.Common.Enums;
 
-var imagePaths = await WebPageParser.GetPageContent("D:\\Projects\\MangaParser\\TempImages", "https://mangalib.me/goodbye-eri/v0/c0?page=", "https://img33.imgslib.link//manga/", 1, 5, new CancellationToken());
+var imagePaths = await WebPageParser.GetPageContent("", "", "", 1, 5, new CancellationToken());
 
-ToFileConverter.ConvertToSelectedFormat("D:\\Projects\\MangaParser\\Done", "Seon2", DocumentFormat.Pdf, imagePaths);
+ToFileConverter.ConvertToSelectedFormat("", "", DocumentFormat.Pdf, imagePaths);
 
